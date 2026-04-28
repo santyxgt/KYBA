139ee589ba9e073367165fffa293de56e9a27529
santyxgt <davidsantiagoguerrerotolosa@gmail.com>
2026-04-27 22:24:55 -0500

    feat: Configuracion del servidor Express a MongoDB
    Se configura el servidor en index.js con Express.
    Se agregan middlewares globales: cors y express.json.
    Se establece la conexión a MongoDB Atlas Usando moongose.
    Se configuran las variables de entorno con dotenv.

---
+++ api/index.js
@@ -0,0 +1,38 @@
+//el index js es el punto de entrada de toda la API cuando corremos "npm run dev" node empieza a leer desde ahi
+// Importar Express y crear el servidor
+// Configurar middlewares globales (como cors y que entienda JSON)
+// Conectarse a MongoDB
+// Poner el servidor a escuchar en un puerto
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+//crea aplicacion express
+
+const app = express();
+//middlewares globales
+
+app.use(cors());
+app.use(express.json);
+
+// Ruta de prueba para verificar que el servidor funciona
+app.get("/", (req, res) => {
+  res.json({ mensaje: "API de Keep Your Boat Afloat funcionando" });
+});
+
+//conexion a MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+    //iniciar el server solo si la bd se logro Conectar
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        `Servidor corriendo en el puerto ${process.env.PORT} || 3000`,
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Error conectando a MongoDB: ", error);
+  });
